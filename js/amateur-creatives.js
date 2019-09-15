ee578e9f213cd69702370843d63df7ae90cabb06
Nico Baguio <nicobaguio41@gmail.com>
2019-09-15 21:28:32 +0800

    Additional edits to header menu

--- js/amateur-creatives.js
+++ js/amateur-creatives.js
@@ -2,3 +2,11 @@ $('.hamburger').on('click', function(e) {
     $(this).toggleClass('is-active');
     $('#page-overlay').toggleClass('reveal');
 })
+
+$('#page-overlay').on('scroll', function () {
+    $('.menu-item a').each( function( a ) {
+        a_hidden = $( this ).offset().top < 165 - $ ( this ).height();
+        $( this ).toggleClass('disabled', a_hidden);
+    })
+
+})
