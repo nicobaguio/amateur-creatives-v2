ae71d19c8ce6392bd1734577b8f8f52167a4f0b5
Nico Baguio <nicobaguio41@gmail.com>
2019-09-25 04:01:24 +0800

    Added card links to mobile view

--- page-home.php
+++ page-home.php
@@ -1,4 +1,24 @@
 <?php get_header(); ?>
 <?php get_template_part( 'template-parts/home/blogposts', 'carousel' ); ?>
 <?php get_template_part( 'template-parts/home/video', 'reel' ); ?>
+<div id="card-link-container">
+    <div id="shop-card-link" class="card-link">
+        <a href="/shop">
+            <svg class="circle-container"><circle class="circle" cx=50% cy=50%></circle></svg>
+            <p>Shop</p>
+        </a>
+    </div>
+    <div id="blogs-card-link" class="card-link">
+        <a href="/blogs">
+            <svg class="circle-container"><circle class="circle" cx=50% cy=50%></circle></svg>
+            <p>Blogs</p>
+        </a>
+    </div>
+    <div id="courses-card-link" class="card-link">
+        <a href="/courses">
+            <svg class="circle-container"><circle class="circle" cx=50% cy=50%></circle></svg>
+            <p>Courses</p>
+        </a>
+    </div>
+</div>
 <?php get_footer(); ?>
