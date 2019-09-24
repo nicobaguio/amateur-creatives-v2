16d6f2350219f2311ab56b1924e3e0d24e560940
Nico Baguio <nicobaguio41@gmail.com>
2019-09-25 02:53:39 +0800

    Added video reel section in home page

--- page-home.php
+++ page-home.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
 <?php get_template_part( 'template-parts/home/blogposts', 'carousel' ); ?>
+<?php get_template_part( 'template-parts/home/video', 'reel' ); ?>
 <?php get_footer(); ?>
