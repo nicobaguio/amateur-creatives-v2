6070738e8d303875c2e18e83cfc7165cb0297008
Nico Baguio <nicobaguio41@gmail.com>
2019-09-25 12:30:34 +0800

    Fixed header and added footer

--- functions.php
+++ functions.php
@@ -42,9 +42,10 @@ if ( function_exists( 'register_nav_menus' ) ) {
 	function ac_nav_menus() {
 		register_nav_menus(
 			array(
-				'header-menu' => 'Header Menu'
-		)
-	);
+                'header-menu' => 'Header Menu',
+                'footer-menu' => 'Footer Menu'
+	    	)
+        );
 }}
 // Register Sidebars
 
