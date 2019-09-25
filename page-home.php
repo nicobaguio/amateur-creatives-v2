9b3db3b8588f1f37c2202346c9f6d78cde5bc968
Nico Baguio <nbaguio@pop-os.localdomain>
2019-09-25 09:32:35 +0800

    Added card link icons to mobile home page

--- page-home.php
+++ page-home.php
@@ -1,22 +1,27 @@
 <?php get_header(); ?>
 <?php get_template_part( 'template-parts/home/blogposts', 'carousel' ); ?>
 <?php get_template_part( 'template-parts/home/video', 'reel' ); ?>
+<?php
+    $shop_icon = get_field('shop_icon');
+    $blog_icon = get_field('blog_icon');
+    $courses_icon = get_field('courses_icon');
+?>
 <div id="card-link-container">
     <div id="shop-card-link" class="card-link">
         <a href="/shop">
-            <svg class="circle-container"><circle class="circle" cx=50% cy=50%></circle></svg>
+                <?php echo wp_get_attachment_image( $shop_icon, 'thumbnail')?>
             <p>Shop</p>
         </a>
     </div>
     <div id="blogs-card-link" class="card-link">
         <a href="/blogs">
-            <svg class="circle-container"><circle class="circle" cx=50% cy=50%></circle></svg>
+                <?php echo wp_get_attachment_image( $blog_icon, 'thumbnail')?>
             <p>Blogs</p>
         </a>
     </div>
     <div id="courses-card-link" class="card-link">
         <a href="/courses">
-            <svg class="circle-container"><circle class="circle" cx=50% cy=50%></circle></svg>
+                <?php echo wp_get_attachment_image( $courses_icon, 'thumbnail')?>
             <p>Courses</p>
         </a>
     </div>
