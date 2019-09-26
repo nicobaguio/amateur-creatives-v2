390e05ae5ef40017a17a250d749ba9af3cbb0b62
Nico Baguio <nicobaguio41@gmail.com>
2019-09-27 07:20:16 +0800

    Fixed search button issue in iPad

--- searchform.php
+++ searchform.php
@@ -1,6 +1,3 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<input type="text" name="s" id="search-text" placeholder="I'm looking for..."/>
-	<button id="search-button">
-		<img src="<?php bloginfo( 'stylesheet_directory' ) ?>/assets/search.svg">
-	</button>
 </form>
