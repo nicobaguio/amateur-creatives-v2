56c83200b559b1c02572b56bc0d1d76b35da90d8
Nico Baguio <nbaguio@pop-os.localdomain>
2019-09-27 01:50:01 +0800

    Fixed JS event listeners to be more robust

--- js/flickity-custom.js
+++ js/flickity-custom.js
@@ -1,17 +1,6 @@
 // console.log('Flick it!')
-
-var flickityActivate = function() {
-    isDesktop = window.matchMedia("(min-width: 1024px)")
-
-    if ( !isDesktop.matches ) {
-        console.log('Mobile...')
-        var flkty = new Flickity('.main-carousel', {
-            contain: true,
-            prevNextButtons: false,
-            imagesLoaded: true
-        })
-    }    
-}
-
-document.addEventListener('DOMContentLoaded', flickityActivate, false);
-document.addEventListener('onorientationchange', flickityActivate, false);
+let flkty = new Flickity('#mobile .main-carousel', {
+    contain: true,
+    prevNextButtons: false,
+    imagesLoaded: true
+})
